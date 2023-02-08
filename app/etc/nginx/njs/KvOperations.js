a52cba87ffbc0e5540d0f8ddacd61ecb323130d1
Tim Quinlan <tim@techfocus.net>
2023-02-08 17:25:04 -0500

    finished up, ready for customer demo

--- app/etc/nginx/njs/KvOperations.js
+++ app/etc/nginx/njs/KvOperations.js
@@ -1,12 +1,18 @@
 async function CheckKv (r) {
   if (!r.variables.myvar) {
+    //
+    //put user info validation code here
+    //
     let newtoken = await r.subrequest("/auth"); 
     var token = newtoken.responseBody;
-    //var token = newtoken.requestText;
     r.variables.myvar = token;
+    r.return(204);
+    return;
   }
   else if (r.variables.myvar) {
+    //
     //put key validation code here
+    //
     r.return(204);
     return;
   }
@@ -14,7 +20,6 @@ async function CheckKv (r) {
     r.return(403);
     return;
   }
-  r.return(204);
 }
 
 export default { CheckKv };
