5c1b124f77cd33e803769f8cad3005150cd8bbda
Tim Quinlan <tim@techfocus.net>
2023-02-08 14:13:58 -0500

    changed requestBody with requestText

--- app/etc/nginx/njs/KvOperations.js
+++ app/etc/nginx/njs/KvOperations.js
@@ -1,11 +1,13 @@
 async function CheckKv (r) {
   if (!r.variables.myvar) {
     let newtoken = await r.subrequest("/auth"); 
-    var token = newtoken.responseBody;
+    var token = newtoken.requestText;
     r.variables.myvar = token;
   }
   else if (r.variables.myvar) {
-    var token = r.variables.myvar;
+    //put key validation code here
+    r.return(204);
+    return;
   }
   else {
     r.return(403);
